a010dbca49f4638209d380db7236b2da8e67107d
Sooraj <sooraj.prajapati@adglobal360.com>
2024-12-20 21:45:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'test';
+
+  apiData: any = null;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    const apiUtl = 'https://jsonplaceholder.typicode.com/users';
+    this.http.get(apiUtl).subscribe(
+      (data) => {
+        console.log(data);
+        this.apiData = data;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
